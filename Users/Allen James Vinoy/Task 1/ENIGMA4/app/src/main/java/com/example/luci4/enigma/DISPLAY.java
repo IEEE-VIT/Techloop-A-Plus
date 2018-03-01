5198de073cfc0405cf68dacbbbcedcbd542266cd
allenjamesvinoy14 <allenjames.vinoy2017@vitstudent.ac.in>
2018-03-01 12:02:18 +0530

    ERROR: The app crashes while transition from one activity to the other

---
+++ Users/Allen James Vinoy/Task 1/ENIGMA4/app/src/main/java/com/example/luci4/enigma/DISPLAY.java
@@ -0,0 +1,40 @@
+package com.example.luci4.enigma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DISPLAY extends AppCompatActivity {
+
+    TextView namedisplay;
+    TextView passworddisplay;
+    TextView phonedisplay;
+
+    String catchname;
+    String catchpassword;
+    String catchphone;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        catchname = getIntent().getExtras().getString("key");
+        catchpassword = getIntent().getExtras().getString("key1");
+        catchphone = getIntent().getExtras().getString("key2");
+
+        namedisplay.setTextSize(30.0f);
+        passworddisplay.setTextSize(30.0f);
+        phonedisplay.setTextSize(30.0f);
+
+        namedisplay.setText(catchname);
+        passworddisplay.setText(catchpassword);
+        phonedisplay.setText(catchpassword);
+
+    }
+}
+
+
